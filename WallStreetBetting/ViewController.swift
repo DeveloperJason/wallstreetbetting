0940b4361628584ba214999fc1ea8450cc64bec7
Jason <jphilpy@gmail.com>
2022-11-19 21:24:00 -0800

    Initial Commit

---
+++ WallStreetBetting/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WallStreetBetting
+//
+//  Created by Jason Philpy on 11/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
